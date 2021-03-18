13dc03494850e3a0a94e6e18580379ec3eac88a8
Pilchenko <pilchenko@icloud.com>
2021-03-18 12:56:20 +0300

    Test_5

--- l5_test.playground/Contents.swift
+++ l5_test.playground/Contents.swift
@@ -1,3 +1,54 @@
 import Cocoa
 
-var str = "Hello, playground"
+enum windows{
+    case open
+    case closed
+}
+enum engine{
+    case on
+    case off
+}
+
+protocol Car {
+    var marka: String {get}
+    var release: Int {get}
+    var engineState: engine {get set}
+    var windowsState: windows {get set}
+}
+extension Car {
+    mutating func changeEngineState(engineState: engine){
+        self.engineState = engineState
+    }
+    mutating func chageWindowsState(windowsState: windows){
+        self.windowsState = windowsState
+    }
+}
+class basicCar: Car{
+    var marka: String
+    var release: Int
+    var engineState: engine = .off
+    var windowsState: windows = .closed
+    init(marka: String, release: Int) {
+        self.marka = marka
+        self.release = release
+    }
+}
+class sportCar: basicCar, CustomStringConvertible{
+    var description: String{
+        return "engine is \(engineState), windows is \(windowsState), release date is \(release)"
+    }
+}
+class trunkCar: basicCar, CustomStringConvertible{
+    var description: String{
+        return "engine is \(engineState), windows is \(windowsState), release date is \(release)"
+    }
+}
+var subaru = sportCar(marka: "Subaru", release: 2019)
+
+subaru.changeEngineState(engineState: .on)
+print(subaru.description)
+
+var ford = trunkCar(marka: "Ford", release: 2020)
+
+ford.chageWindowsState(windowsState: .open)
+print(ford.description)
